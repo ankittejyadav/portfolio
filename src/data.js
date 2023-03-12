9140c092da655e686a335b36308051106a00c471
Ankit Yadav <ankittejyadav@gmail.com>
2023-03-11 21:46:30 -0500

    one

---
+++ src/data.js
@@ -0,0 +1,67 @@
+export const projects = [
+  {
+    title: "React Reserve",
+    subtitle: "MERN Stack",
+    description:
+      "Lorem ipsum dolor sitttttrttttt amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-1.gif",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "React Tracks",
+    subtitle: "React and Python",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-2.gif",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "DevChat",
+    subtitle: "React and Firebase",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-3.gif",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "Epic Todo App",
+    subtitle: "React Hooks",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-4.gif",
+    link: "https://reactbootcamp.com",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "I supervised Ankit at CARB-X for 10 months. During this time, Ankit supported a range of projects for our communications, data and project management teams. He always exhibited a positive attitude, and a willingness to troubleshoot and work independently while meeting project deadlines.",
+    // image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Robin Berghaus",
+    company: "CARB-X",
+  },
+  {
+    quote:
+      "Ankit's knowledge in Data Science, Python, and Pandas paved the way until the end. Ankit managed to solve complicated problems in professional way. One of the biggest challenge was when he was tasked to build the workflow for containerizing our codebase in Docker. Ankit tackled this challenge with determination and was able to successfully build the workflow for containerizing our codebase in Docker.",
+    // image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Faza Hikmatullah",
+    company: "All in Energy",
+  },
+  {
+    quote:
+      "Ankit was able to successfully implement other new skills for our team by setting up a cloud data pipeline using AWS S3, AWS Glue, AWS Athena, and AWS Quicksight, once again reading documentation, implementing different methods, and sharing the best method with the team during team meetings. After setting up that pipeline, he took lead on designing and implementing the foundation of our analytical dashboard to track the growth of our product using the BI AWS tool Quicksight. Ankit was able to contribute to the team on a strong technical level.",
+    // image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Joshua Minuskin",
+    company: "Pre Health",
+  },
+];
+
+export const skills = [
+  "Python",
+  'SQL',
+  "React",
+  "JavaScript",
+  "Node",
+  "Java",
+];
